84bf265a926c77fc0500718de80ea0e77be809d6
Shiva A.Lindi <azizpour.shiva@gmail.com>
2021-01-14 13:20:06 +0100

    fft and welch compared, welch is too sensitive to length of the signal

--- Oscillation.py
+++ Oscillation.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import timeit
-from numpy.fft import rfft,fft
+from numpy.fft import rfft,fft, fftfreq
 from scipy import signal
+from tempfile import TemporaryFile
+#from mpl_toolkits.mplot3d import Axes3D
 #from scipy.ndimage.filters import generic_filter
 
 N_sim = 1000
@@ -49,80 +51,9 @@ d_Str = 200 # duration of external input to Str
 t_ext_to_Ctx = (t_mvt-D_mvt/2,t_mvt+D_mvt/2)
 t_ext_to_Str = (t_mvt-D_mvt/2,t_mvt-D_mvt/2+d_Str)
 t_list = np.arange(int(t_sim/dt))
-duration_mvt = [int(t_mvt/dt), int((t_mvt+D_mvt)/dt)]
-def freq_from_fft(sig):
-    """
-    Estimate frequency from peak of FFT
-    """
-    # Compute Fourier transform of windowed signal
-    windowed = sig * signal.blackmanharris(len(sig))
-    f = rfft(windowed)
+duration_mvt = [int((t_mvt+ max(T[('GPe', 'Str')],T[('STN', 'Ctx')]))/dt), int((t_mvt+D_mvt+max(T[('GPe', 'Str')],T[('STN', 'Ctx')]))/dt)]
 
-    # Find the peak and interpolate to get a more accurate peak
-    i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
-#    true_i = parabolic(log(abs(f)), i)[0]
-
-    # Convert to equivalent frequency
-#    return fs * true_i / len(windowed)
-    return i
-
-def cut_plateau(sig,epsilon = 10**(-2)):
-    variation = (sig-np.average(sig))**2
-    ind = np.where(variation >= epsilon)
-#    plt.plot(variation)
-#    plt.axvline(ind[0][0])
-    return ind[0]
-
-def freq_from_welch(sig,dt):
-    """
-    Estimate frequency with Welch method
-    """
-    fs = 1/(dt/1000)
-    [ff,pxx] = signal.welch(sig,axis=0,nperseg=int(fs),fs=fs)#,nfft=1024)
-    
-    return ff[np.argmax(pxx)]
-
-def mvt_grad_ext_input(D_mvt, t_mvt, delay, H0, t_series):
-    ''' a gradually increasing deacreasing input mimicing movement'''
-
-    H = H0*np.cos(2*np.pi*(t_series-t_mvt)/D_mvt)**2
-    ind = np.logical_or(t_series < t_mvt + delay, t_series > t_mvt + D_mvt + delay)
-    H[ind] = 0
-    return H
-def mvt_step_ext_input(D_mvt,t_mvt,delay, H0, t_series):
-    
-    H = H0*np.ones_like(t_series)
-    ind = np.logical_or(t_series < t_mvt + delay, t_series > t_mvt + D_mvt + delay)
-    H[ind] = 0
-    return H
-#plt.plot(t_list, mvt_ext_input(D_mvt,t_mvt,H0, t_list/dt))
-
-def calculate_number_of_connections(N_sim,N_real,K_real):
-    '''calculate number of connections in the scaled network.'''
-    KK = K_real.copy()
-    for k, v in K_real.items():
-        KK[k] = int(1/(1/v-1/N_real[k[1]]+1/N_sim[k[0]]))
-    return KK
-
-def transfer_func(Threshold, gain, x):
-    ''' a transfer function that grows linearly for positive values 
-    of input higher than the threshold'''
-#    print(x.shape)
-#    print(np.maximum(np.zeros_like(x),x))
-    return gain* np.maximum(np.zeros_like(x), (x - Threshold))
-    
-def build_connection_matrix(n_receiving,n_projecting,n_connections):
-    ''' return a matrix with Jij=0 or 1. 1 showing a projection from neuron j in projectin population to neuron i in receiving'''
-    # produce a matrix listing received projections for each neuron in row i
-    projection_list = np.random.rand(n_receiving, n_projecting).argpartition(n_connections,axis=1)[:,:n_connections]
-#    print(projection_list)
-    JJ = np.zeros((n_receiving, n_projecting))
-    rows = ((np.ones((n_connections,n_receiving))*np.arange(n_receiving)).T).flatten().astype(int)
-    cols = projection_list.flatten().astype(int)
-    JJ[rows,cols] = 1
-    return JJ
-dictfilt = lambda x, y: dict([ (i,x[i]) for i in x if i in set(y) ])
-#build_connection_matrix(4,10,2)
+#%%
 class Nucleus:
 
     def __init__(self, N, A, name, T, t_sim, dt, tau, n_trans_types, rest_ext_input, receiving_from_list):
@@ -186,7 +117,35 @@ class Nucleus:
             n_connections = K[(self.name, projecting)]
             J[(self.name, projecting)] = build_connection_matrix(self.n, N[projecting], n_connections)
 
-#%%
+def freq_from_fft(sig,dt):
+    """
+    Estimate frequency from peak of FFT
+    """
+    # Compute Fourier transform of windowed signal
+#    windowed = sig * signal.blackmanharris(len(sig))
+#    f = rfft(windowed)
+    f = rfft(sig)
+    N = len(sig)
+    freq = fftfreq(N, dt)#[:N//2]
+    plt.figure()
+    plt.semilogy(freq[:N//2], f[:N//2])
+    # Find the peak and interpolate to get a more accurate peak
+    peak_freq = freq[np.argmax(abs(f))]  # Just use this for less-accurate, naive version
+#    true_i = parabolic(log(abs(f)), i)[0]
+
+    # Convert to equivalent frequency
+#    return fs * true_i / len(windowed)
+    return peak_freq
+
+def freq_from_welch(sig,dt):
+    """
+    Estimate frequency with Welch method
+    """
+    fs = 1/(dt)
+    [ff,pxx] = signal.welch(sig,axis=0,fs=fs,nperseg=int(fs))
+    plt.semilogy(ff,pxx)
+    return ff[np.argmax(pxx)]
+
 def run():
 
     
@@ -215,7 +174,7 @@ def run():
     print("t = ", stop - start)
     return 
     
-def plot(GPe, STN):    
+def plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt):    
     plot_start = int(5/dt)
     
     plt.figure(1)    
@@ -233,78 +192,139 @@ def plot(GPe, STN):
     plt.ylabel("firing rate (spk/s)")
     plt.legend()
   
+def sweep_time_scales():
+    n = 10
+    GABA_A = np.linspace(5,17,n)
+    GABA_B = np.linspace(150,300,n)
+    Glut = np.linspace(0.5,10,n)
+    STN_freq = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut)))
+    GPe_freq = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut)))
+    tau_mat = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut),3))
+    count = 0
+    for gaba_b in GABA_B:
+        for gaba_a in GABA_A:
+            for glut in Glut:
+                
+                GPe.tau = {'GABA-A' : gaba_a, 'GABA-B' : gaba_b}
+                STN.tau = {'Glut': glut} 
     
+                run()
+                tau_mat[count,:] = [gaba_a, gaba_b, glut]
+                sig_STN = STN.pop_act[duration_mvt[0]:duration_mvt[1]] - np.average(STN.pop_act[duration_mvt[0]:duration_mvt[1]])
+#                STN_freq[count] = freq_from_welch(sig_STN[cut_plateau(sig_STN)],dt/1000)
+                STN_freq[count] = freq_from_fft(sig_STN[cut_plateau(sig_STN)],dt/1000)
+
+                sig_GPe = GPe.pop_act[duration_mvt[0]:duration_mvt[1]] - np.average(GPe.pop_act[duration_mvt[0]:duration_mvt[1]])
+#                GPe_freq [count] = freq_from_welch(sig_GPe[cut_plateau(sig_GPe)],dt/1000)
+                GPe_freq [count] = freq_from_fft(sig_GPe[cut_plateau(sig_GPe)],dt/1000)
+
+                count +=1
+    
+    
+#    outfile = TemporaryFile()
+    np.savez('data.npz', tau_mat = tau_mat, GPe = GPe_freq, STN = STN_freq )
+    file = np.load('data.npz')
+    #X,Y = np.meshgrid(tau_mat[:,0],tau_mat[:,1])
+    tau_mat = file['tau_mat']
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    x = tau_mat[:,0]
+    y = tau_mat[:,1]
+    z = tau_mat[:,2]
+    c = file['GPe']
+    
+    img = ax.scatter(x, y, z, c=c, cmap=plt.hot())
+    ax.set_xlabel('GABA-A')
+    ax.set_ylabel('GABA-B')
+    ax.set_zlabel('Glut')
+    clb = fig.colorbar(img)
+    clb.set_label('frequency', labelpad=-40, y=1.05, rotation=0)
+    plt.show()
+    
+def cut_plateau(sig,epsilon = 10**(-2)):
+    variation = (sig-np.average(sig))**2
+    ind = np.where(variation >= epsilon)
+#    plt.plot(variation)
+#    plt.axvline(ind[0][0])
+    return ind[0]
+
+def mvt_grad_ext_input(D_mvt, t_mvt, delay, H0, t_series):
+    ''' a gradually increasing deacreasing input mimicing movement'''
+
+    H = H0*np.cos(2*np.pi*(t_series-t_mvt)/D_mvt)**2
+    ind = np.logical_or(t_series < t_mvt + delay, t_series > t_mvt + D_mvt + delay)
+    H[ind] = 0
+    return H
+def mvt_step_ext_input(D_mvt,t_mvt,delay, H0, t_series):
+    
+    H = H0*np.ones_like(t_series)
+    ind = np.logical_or(t_series < t_mvt + delay, t_series > t_mvt + D_mvt + delay)
+    H[ind] = 0
+    return H
+#plt.plot(t_list, mvt_ext_input(D_mvt,t_mvt,H0, t_list/dt))
+
+def calculate_number_of_connections(N_sim,N_real,K_real):
+    '''calculate number of connections in the scaled network.'''
+    KK = K_real.copy()
+    for k, v in K_real.items():
+        KK[k] = int(1/(1/v-1/N_real[k[1]]+1/N_sim[k[0]]))
+    return KK
+
+def transfer_func(Threshold, gain, x):
+    ''' a transfer function that grows linearly for positive values 
+    of input higher than the threshold'''
+#    print(x.shape)
+#    print(np.maximum(np.zeros_like(x),x))
+    return gain* np.maximum(np.zeros_like(x), (x - Threshold))
+    
+def build_connection_matrix(n_receiving,n_projecting,n_connections):
+    ''' return a matrix with Jij=0 or 1. 1 showing a projection from neuron j in projectin population to neuron i in receiving'''
+    # produce a matrix listing received projections for each neuron in row i
+    projection_list = np.random.rand(n_receiving, n_projecting).argpartition(n_connections,axis=1)[:,:n_connections]
+#    print(projection_list)
+    JJ = np.zeros((n_receiving, n_projecting))
+    rows = ((np.ones((n_connections,n_receiving))*np.arange(n_receiving)).T).flatten().astype(int)
+    cols = projection_list.flatten().astype(int)
+    JJ[rows,cols] = 1
+    return JJ
+dictfilt = lambda x, y: dict([ (i,x[i]) for i in x if i in set(y) ])
+#build_connection_matrix(4,10,2)
+#%%  
 K = calculate_number_of_connections(N,N_real,K_real)
 GPe = Nucleus(N, A, 'GPe', T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, ['STN', 'GPe'])
-
 #GPe = Nucleus(N, A, 'GPe', T, t_sim, dt, tau, ['GABA-A','GABA-B'], rest_ext_input, ['STN', 'GPe'])
 STN = Nucleus(N, A, 'STN', T, t_sim, dt, tau, ['Glut'], rest_ext_input, ['GPe'])
+
 run()
-plot(GPe,STN)
-#%%
-    
-n = 10
-GABA_A = np.linspace(5,17,n)
-GABA_B = np.linspace(150,300,n)
-Glut = np.linspace(0.5,10,n)
-STN_freq = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut)))
-GPe_freq = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut)))
-tau_mat = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut),3))
-count = 0
-for gaba_b in GABA_B:
-    for gaba_a in GABA_A:
-        for glut in Glut:
-            
-            GPe.tau = {'GABA-A' : gaba_a, 'GABA-B' : gaba_b}
-            STN.tau = {'Glut': glut} 
+plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt)
+#sweep_time_scales()
 
-            run()
-            tau_mat[count,:] = [gaba_a, gaba_b, glut]
-            sig_STN = STN.pop_act[duration_mvt[0]:duration_mvt[1]]
-            STN_freq [count] = freq_from_welch(sig_STN[cut_plateau(sig_STN)],dt)
-            sig_GPe = GPe.pop_act[duration_mvt[0]:duration_mvt[1]]
-            GPe_freq [count] = freq_from_welch(sig_GPe[cut_plateau(sig_GPe)],dt)
-            count +=1
 
-from tempfile import TemporaryFile
-from mpl_toolkits.mplot3d import Axes3D
-outfile = TemporaryFile()
-np.savez('data.npz', tau_mat = tau_mat, GPe = GPe_freq, STN = STN_freq )
-file = np.load('data.npz')
-file.files
-#X,Y = np.meshgrid(tau_mat[:,0],tau_mat[:,1])
+#%% Scribble
+
+#def check_freq_detection(nucleus, t_list, dt)
+tt = t_list[duration_mvt[0]:duration_mvt[1]]*dt/1000
+#N = 1000; T = 1.0 / 800.0
+#x = np.linspace(0.0, N*T, N, endpoint=False)
+sig1  = np.sin(10.0 * 2.0*np.pi*tt)*np.exp(-10*tt)+100
+#indices = np.nonzero((sig1[1:] >= 0) & (sig1[:-1] < 0))[0] # zero crossing
+sig2 = STN.pop_act[duration_mvt[0]:duration_mvt[1]]
 plt.figure(2)
-plt.axes(projection='3d')
-tau_mat = file['tau_mat']
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-x = tau_mat[:,0]
-y = tau_mat[:,1]
-z = tau_mat[:,2]
-c = file['GPe']
-
-img = ax.scatter(x, y, z, c=c, cmap=plt.hot())
-ax.set_xlabel('GABA-A')
-ax.set_ylabel('GABA-B')
-ax.set_zlabel('Glut')
-clb = fig.colorbar(img)
-clb.set_label('frequency', labelpad=-40, y=1.05, rotation=0)
-plt.show()
-#tt = t_list[t_freq[0]:t_freq[1]]
-#sig1  = np.sin(2*np.pi*10*tt)#*np.exp(-0.015*t_list[t_freq[0]:t_freq[1]])
-
-#-np.average(STN.pop_act[t_freq[0]:t_freq[1]])+10
-
-#sig_filtered = generic_filter(sig2, np.std, size=10)
-#plt.plot(sig1)
-#plt.plot(sig2)
+plt.plot(sig2)
+plt.plot(sig2[cut_plateau(sig2)])
+plt.plot(sig1[cut_plateau(sig1)])
 #plt.plot(sig_filtered)
 #fs = 1/(dt/1000)
 #[ff,pxx] = signal.welch(sig2,axis=0,nperseg=int(fs),fs=fs)#,nfft=1024)
 #plt.plot(ff,pxx)
 #ff[np.argmax(pxx)]
+
+freq_from_fft((sig2-np.average(sig2))[[cut_plateau(sig2)]],dt/1000)
+freq_from_welch((sig2[10:]-np.average(sig2[10:]))[[cut_plateau(sig2[10:])]],dt/1000)
+freq_from_fft(sig2[cut_plateau(sig2)],dt)
+freq_from_welch(sig2[cut_plateau(sig2)],dt)
 #f1 = fft(sig1)
 #f2 = fft(sig2)
 #plt.figure(2)
