e4276e3256f494e11bf91cd0c733e3a64865ae8f
Shiva <you@example.com>
2021-01-19 15:03:37 +0100

    plateau detection with moving average variance threshold

--- Oscillation.py
+++ Oscillation.py
@@ -26,7 +26,7 @@ K_real = { ('STN', 'GPe'): 883, # Baufreton et al. (2009)
            ('GPe', 'GPe'): 650} # Hegeman et al. (2017)
 T = { ('STN', 'GPe'): 4, # Fujimoto & Kita (1993)
       ('GPe', 'STN'): 2, # kita & Kitai (1991)
-      ('GPe', 'GPe'): 1.5,
+      ('GPe', 'GPe'): 1.5, # estimate
       ('GPe', 'Str'): 5, # Kita & Kitai (1991)
       ('STN', 'Ctx'): 5.5} # kita & Kita (2011)
     # transmission delay in ms
@@ -121,19 +121,28 @@ def freq_from_fft(sig,dt):
     # Compute Fourier transform of windowed signal
 #    windowed = sig * signal.blackmanharris(len(sig))
 #    f = rfft(windowed)
-    f = rfft(sig)
     N = len(sig)
-    freq = fftfreq(N, dt)#[:N//2]
-#    plt.figure()
-#    plt.semilogy(freq[:N//2], f[:N//2])
-    # Find the peak and interpolate to get a more accurate peak
-    peak_freq = freq[np.argmax(abs(f))]  # Just use this for less-accurate, naive version
-#    true_i = parabolic(log(abs(f)), i)[0]
+    if N == 0:
+        return 0
+    else:
+        f = rfft(sig)
+        freq = fftfreq(N, dt)#[:N//2]
+    #    plt.figure()
+    #    plt.semilogy(freq[:N//2], f[:N//2])
+        # Find the peak and interpolate to get a more accurate peak
+        peak_freq = freq[np.argmax(abs(f))]  # Just use this for less-accurate, naive version
+    #    true_i = parabolic(log(abs(f)), i)[0]
+    
+        # Convert to equivalent frequency
+    #    return fs * true_i / len(windowed)
 
-    # Convert to equivalent frequency
-#    return fs * true_i / len(windowed)
-    return peak_freq
+        return peak_freq
 
+def find_peaks(sig,dt):
+    
+    peaks, _ = signal.find_peaks(sig)
+    frequency = np.shift()
+    
 def freq_from_welch(sig,dt):
     """
     Estimate frequency with Welch method
@@ -169,25 +178,28 @@ def run():
     print("t = ", stop - start)
     return 
     
-def plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt, title = ""):    
+def plot( GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt, plot_ob, title = "", n_subplots = 1):    
     plot_start = int(5/dt)
-    
-    plt.figure()    
-    plt.plot(t_list[plot_start:]*dt,GPe.pop_act[plot_start:],label = "GPe", c = 'r')
-    plt.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A['GPe'], '--', c = 'r', alpha=0.8 )
-    plt.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A_mvt['GPe'], '--', c = 'r', alpha=0.15 )
+    if plot_ob == None:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plot_ob
+     
+    ax.plot(t_list[plot_start:]*dt,GPe.pop_act[plot_start:],label = "GPe", c = 'r')
+    ax.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A['GPe'], '--', c = 'r', alpha=0.8 )
+    ax.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A_mvt['GPe'], '--', c = 'r', alpha=0.15 )
     
       
-    plt.plot(t_list[plot_start:]*dt,STN.pop_act[plot_start:], label = "STN", c = 'k')
-    plt.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A['STN'], '--', c = 'k', alpha=0.8 )
-    plt.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A_mvt['STN'], '--', c = 'k', alpha=0.15 )
-    plt.axvspan(t_mvt, t_mvt+D_mvt, alpha=0.2, color='lightskyblue')
+    ax.plot(t_list[plot_start:]*dt,STN.pop_act[plot_start:], label = "STN", c = 'k')
+    ax.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A['STN'], '--', c = 'k', alpha=0.8 )
+    ax.plot(t_list[plot_start:]*dt, np.ones_like(t_list[plot_start:])*A_mvt['STN'], '--', c = 'k', alpha=0.15 )
+    ax.axvspan(t_mvt, t_mvt+D_mvt, alpha=0.2, color='lightskyblue')
     
-    plt.title(title)
-    plt.xlabel("time (ms)")
-    plt.ylabel("firing rate (spk/s)")
-    plt.legend()
-  
+    plt.title(title, fontsize = 18)
+    plt.xlabel("time (ms)", fontsize = 10)
+    plt.ylabel("firing rate (spk/s)", fontsize = 10)
+    plt.legend(fontsize = 5)
+    ax.tick_params(axis='both', which='major', labelsize=10)
 def sweep_time_scales(GABA_A, GABA_B, Glut, dt, filename):
 
     STN_freq = np.zeros((len(GABA_A)*len(GABA_B)*len(Glut)))
@@ -247,13 +259,7 @@ def sweep_time_scales_one_GABA(inhibitory_trans,inhibitory_series, Glut, dt, fil
     np.savez(filename, tau_mat = tau_mat, GPe = GPe_freq, STN = STN_freq )
     
 
-def cut_plateau(sig,epsilon = 10**(-2)):
-    ''' return indices before a plateau '''
-    variation = (sig-np.average(sig))**2
-    ind = np.where(variation >= epsilon)
-#    plt.plot(variation)
-#    plt.axvline(ind[0][0])
-    return ind[0]
+
 
 def mvt_grad_ext_input(D_mvt, t_mvt, delay, H0, t_series):
     ''' a gradually increasing deacreasing input mimicing movement'''
@@ -295,21 +301,21 @@ def build_connection_matrix(n_receiving,n_projecting,n_connections):
     return JJ
 dictfilt = lambda x, y: dict([ (i,x[i]) for i in x if i in set(y) ])
 
-def scatter_3d_plot(x,y,z,c, nucleus, c_upper_limit, c_lower_limit, label):
+def scatter_3d_plot(x,y,z,c, title, c_upper_limit, c_lower_limit, label):
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
     ind = np.logical_and(c<=c_upper_limit, c>=c_lower_limit)
-    print(ind)
+#    print(ind)
     img = ax.scatter(x[ind], y[ind], z[ind], c=c[ind], cmap=plt.hot(),lw = 1,edgecolor = 'k')
     
     ax.set_xlabel(label[0])
     ax.set_ylabel(label[1])
     ax.set_zlabel(label[2])
-    ax.set_title(nucleus)
+    ax.set_title(title)
     clb = fig.colorbar(img)
-    clb.set_label('frequency', labelpad=-40, y=1.05, rotation=0)
+    clb.set_label(label[3], labelpad=-40, y=1.05, rotation=0)
     plt.show()
 #    fig = plt.figure(figsize=(6,6))
 #    ax = Axes3D(fig)
@@ -319,8 +325,14 @@ def scatter_3d_plot(x,y,z,c, nucleus, c_upper_limit, c_lower_limit, label):
 #    ax.set_zlabel('Glut')
 #    fig.colorbar(surf, shrink=0.5, aspect=5)
 #build_connection_matrix(4,10,2)
+   
+def max_non_empty_array(array):
+    if len(array) == 0:
+        return 0
+    else:
+        return np.max(array)
     
-    
+
 def synaptic_weight_space_exploration(g_inh_list, g_exit_list, GPe, STN, duration_mvt, duration_base):
     
     n = len(g_inh_list)
@@ -330,55 +342,121 @@ def synaptic_weight_space_exploration(g_inh_list, g_exit_list, GPe, STN, duratio
                 'perc_t_oscil_base': np.zeros((int(m*n))), 'perc_t_oscil_mvt': np.zeros((int(m*n)))}
     GPe_prop = {'base_f' : np.zeros((int(m*n))), 'mvt_f' : np.zeros((int(m*n))),
                 'perc_t_oscil_base': np.zeros((int(m*n))), 'perc_t_oscil_mvt': np.zeros((int(m*n)))}
+
     count  = 0
+    fig = plt.figure()
     for g_inh in g_inh_list:
         for g_exit in g_exit_list:
             GPe.synaptic_weight[('GPe', 'STN')] = g_exit
-            GPe.synaptic_weight[('GPe', 'GPe')] = g_inh*0.5
+#            GPe.synaptic_weight[('GPe', 'GPe')] = 0#g_inh*0.5
             STN.synaptic_weight[('STN','GPe')] = g_inh
             g_mat[count,:] = [g_exit, g_inh, g_inh*0.5]
             run()
-            sig_STN_mvt = STN.pop_act[duration_mvt[0]:duration_mvt[1]] - np.average(STN.pop_act[duration_mvt[0]:duration_mvt[1]])
+            x1_gp_mvt = np.argmax(GPe.pop_act[int(t_mvt/dt):int((t_mvt+D_mvt)/dt)])+int(t_mvt/dt)
+            x1_stn_mvt = np.argmax(STN.pop_act[int(t_mvt/dt):int((t_mvt+D_mvt)/dt)])+int(t_mvt/dt)
+            x2_mvt = duration_mvt[1]
+            x1_gp_base = np.argmax(GPe.pop_act[0:int(t_mvt/dt)])
+            x1_stn_base = np.argmax(STN.pop_act[0:int(t_mvt/dt)])
+            x2_base = duration_base[1]
+            sig_STN_mvt = STN.pop_act[x1_stn_mvt:x2_mvt] - np.average(STN.pop_act[x1_stn_mvt:x2_mvt])
             cut_sig_ind_mvt = cut_plateau(sig_STN_mvt)
-            STN_prop[('perc_t_oscil_mvt')][count] = np.max(cut_sig_ind_mvt)/len(sig_STN_mvt)*100
+            
+            STN_prop[('perc_t_oscil_mvt')][count] = max_non_empty_array(cut_sig_ind_mvt)/len(sig_STN_mvt)*100
             STN_prop[('mvt_f')][count] = freq_from_fft(sig_STN_mvt[cut_sig_ind_mvt],dt/1000)
 
-            sig_STN_base = STN.pop_act[duration_base[0]:duration_base[1]] - np.average(STN.pop_act[duration_base[0]:duration_base[1]])
+            sig_STN_base = STN.pop_act[x1_stn_base:x2_base] - np.average(STN.pop_act[x1_stn_base:x2_base])
             cut_sig_ind_base = cut_plateau(sig_STN_base)
-            STN_prop[('perc_t_oscil_base')][count] = np.max(cut_sig_ind_base)/len(sig_STN_base)*100
+            STN_prop[('perc_t_oscil_base')][count] = max_non_empty_array(cut_sig_ind_base)/len(sig_STN_base)*100
             STN_prop[('base_f')][count] = freq_from_fft(sig_STN_base[cut_sig_ind_base],dt/1000)
 
-            sig_GPe_mvt = GPe.pop_act[duration_mvt[0]:duration_mvt[1]] - np.average(GPe.pop_act[duration_mvt[0]:duration_mvt[1]])
+            sig_GPe_mvt = GPe.pop_act[x1_gp_mvt:x2_mvt] - np.average(GPe.pop_act[x1_gp_mvt:x2_mvt])
             cut_sig_ind_mvt = cut_plateau(sig_GPe_mvt)
-            GPe_prop[('perc_t_oscil_mvt')][count] = np.max(cut_sig_ind_mvt)/len(sig_GPe_mvt)*100
+            GPe_prop[('perc_t_oscil_mvt')][count] = max_non_empty_array(cut_sig_ind_mvt)/len(sig_GPe_mvt)*100
             GPe_prop[('mvt_f')][count] = freq_from_fft(sig_GPe_mvt[cut_sig_ind_mvt],dt/1000)
 
-            sig_GPe_base = GPe.pop_act[duration_base[0]:duration_base[1]] - np.average(GPe.pop_act[duration_base[0]:duration_base[1]])
+            sig_GPe_base = GPe.pop_act[x1_gp_base:x2_base] - np.average(GPe.pop_act[x1_gp_base:x2_base])
             cut_sig_ind_base = cut_plateau(sig_GPe_base)
-            GPe_prop[('perc_t_oscil_base')][count] = np.max(cut_sig_ind_base)/len(sig_GPe_base)*100
+            GPe_prop[('perc_t_oscil_base')][count] = max_non_empty_array(cut_sig_ind_base)/len(sig_GPe_base)*100
             GPe_prop[('base_f')][count] = freq_from_fft(sig_GPe_base[cut_sig_ind_base],dt/1000)
-
-            plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt, title = str(g_inh)+' '+str(g_exit))
+            
+            ax = fig.add_subplot(n,m,count+1)
+            plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt,[fig, ax], title = r"$G_{STN-GPe}$ = "+ str(round(g_inh,2))+r' $G_{GPe-STN}$ ='+str(round(g_exit,2)), n_subplots = int(n*m))
+            plt.title( r"$G_{STN-GPe}$ = "+ str(round(g_inh,2))+r' $G_{GPe-STN}$ ='+str(round(g_exit,2)), fontsize = 5)
+            plt.xlabel("time (ms)", fontsize = 5)
+            plt.ylabel("firing rate (spk/s)", fontsize = 5)
+            plt.legend(fontsize = 2)
+            ax.tick_params(axis='both', which='major', labelsize=2)
             count +=1
             print(count, "from", int(m*n))
             
+    fig.tight_layout() # Or equivalently,  "plt.tight_layout()"
+
     return g_mat, GPe_prop, STN_prop
-        
-        
+       
+def cut_plateau(sig,epsilon_std = 10**(-2), epsilon = 10**(-2), window = 40):
+    ''' return indices before a plateau '''
+    ## filtering based on data variance from mean value
+#    variation = (sig-np.average(sig))**2
+#    ind = np.where(variation >= epsilon_std) 
+#    return ind[0]
+    ## filtering based on where the first and second derivatives are zero. Doesn't work with noise
+#    derivative = np.diff(sig)
+#    derivative_2 = np.diff(derivative)
+#    ind = np.logical_and(np.abs(derivative[:-1])<epsilon,np.abs(derivative_2) < epsilon )
+#    plateau_start = np.max(np.where(~ind))
+#    return np.arange(plateau_start)
+    ##
+    rolling_var = np.var(rolling_window(sig, window), axis=-1)
+    ind = np.where(rolling_var > epsilon)
+    return ind[0]
+def moving_average_array(X, n):
+	'''Return the moving average over X with window n without changing dimesions of X'''
+
+	z2= np.cumsum(np.pad(X, (n,0), 'constant', constant_values=0))
+	z1 = np.cumsum(np.pad(X, (0,n), 'constant', constant_values=X[-1]))
+	return (z1-z2)[(n-1):-1]/n
+
+
+def rolling_window(a, window):
+    pad = np.ones(len(a.shape), dtype=np.int32)
+    pad[-1] = window-1
+    pad = list(zip(pad, np.zeros(len(a.shape), dtype=np.int32)))
+    a = np.pad(a, pad,mode='reflect')
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
 #%%  
+#G = { ('STN', 'GPe'): g_mat[15,1] ,
+#  ('GPe', 'STN'): g_mat[15,0], 
+#  ('GPe', 'GPe'): 0} # synaptic weight
+##G[('GPe', 'GPe')] = 0.5* G[('STN', 'GPe')]
+
 K = calculate_number_of_connections(N,N_real,K_real)
-GPe = Nucleus(N, A, 'GPe', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, ['STN', 'GPe'])
+GPe = Nucleus(N, A, 'GPe', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, ['STN'])#, 'GPe'])
 #GPe = Nucleus(N, A, 'GPe', T, t_sim, dt, tau, ['GABA-A','GABA-B'], rest_ext_input, ['STN', 'GPe'])
 STN = Nucleus(N, A, 'STN', G, T, t_sim, dt, tau, ['Glut'], rest_ext_input, ['GPe'])
 
 run()
-plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt)
-n_inh = 3 ; n_exit = 3
+
+plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt,plot_ob = None)
+n_inh = 10 ; n_exit = 10
 g_inh_list = np.linspace(-3, -0.1, n_inh)
-g_exit_list = np.linspace(0.1, 3, n_inh)
+g_exit_list = np.linspace(0.1, 3, n_exit)
 g_mat, GPe_prop, STN_prop = synaptic_weight_space_exploration(g_inh_list, g_exit_list, GPe, STN, duration_mvt, duration_base)
-scatter_3d_plot(g_mat[:,0],g_mat[:,1],STN_prop['perc_t_oscil_base'],STN_prop['perc_t_oscil_base'], 'STN', np.max(STN_prop['perc_t_oscil_base']), np.min(STN_prop['perc_t_oscil_base']), ['GPe-STN', 'STN-GPe', '% basal oscillatory period'])
-#%% # time scale parameter space with frequency of transient 
+#scatter_3d_plot(g_mat[:,0],g_mat[:,1],STN_prop['perc_t_oscil_base'],STN_prop['perc_t_oscil_base'], 'STN', np.max(STN_prop['perc_t_oscil_base']), np.min(STN_prop['perc_t_oscil_base']), ['GPe-STN', 'STN-GPe', '% basal oscillatory period',  '% basal oscillatory period'])
+#scatter_3d_plot(g_mat[:,0],g_mat[:,1],STN_prop['perc_t_oscil_mvt'],STN_prop['perc_t_oscil_mvt'], 'STN', np.max(STN_prop['perc_t_oscil_mvt']), np.min(STN_prop['perc_t_oscil_mvt']), ['GPe-STN', 'STN-GPe', '% mvt oscillatory period', '% mvt oscillatory period'])
+#scatter_3d_plot(g_mat[:,0],g_mat[:,1],GPe_prop['perc_t_oscil_mvt'],GPe_prop['perc_t_oscil_mvt'], 'GPe', np.max(GPe_prop['perc_t_oscil_mvt']), np.min(GPe_prop['perc_t_oscil_mvt']), ['GPe-STN', 'STN-GPe', '% mvt oscillatory period', '% mvt oscillatory period'])
+#scatter_3d_plot(g_mat[:,0],g_mat[:,1],STN_prop['mvt_f'],STN_prop['mvt_f'], 'STN', np.max(STN_prop['mvt_f']), np.min(STN_prop['mvt_f']), ['GPe-STN', 'STN-GPe', 'mvt freq', 'mvt freq'])
+fig = plt.figure()
+img = plt.scatter(g_mat[:,0],-g_mat[:,1], c = STN_prop['perc_t_oscil_base'], cmap=plt.hot(),lw = 1,edgecolor = 'k')
+plt.xlabel(r'$G_{GPe-STN}$')
+plt.ylabel(r'$G_{STN-GPe}$')
+plt.title('STN')
+clb = fig.colorbar(img)
+clb.set_label('% oscillation period', labelpad=-40, y=1.05, rotation=0)
+plt.show()
+#%% # time scale parameter space with frequency of transient oscillations at steady state
 n = 8
 GABA_A = np.linspace(5,20,n)
 GABA_B = np.linspace(150,300,4)
@@ -419,13 +497,27 @@ scatter_3d_plot(x,y,z, file['GPe'],'GPe', np.max(z), np.min(z),['GABA_B','Glut',
 tt = t_list[duration_mvt[0]:duration_mvt[1]]*dt/1000
 #N = 1000; T = 1.0 / 800.0
 #x = np.linspace(0.0, N*T, N, endpoint=False)
-sig1  = np.sin(10.0 * 2.0*np.pi*tt)*np.exp(-10*tt)+100
+#sig1  = np.sin(10.0 * 2.0*np.pi*tt)*np.exp(-10*tt)+100
 #indices = np.nonzero((sig1[1:] >= 0) & (sig1[:-1] < 0))[0] # zero crossing
 sig2 = STN.pop_act[duration_mvt[0]:duration_mvt[1]]
-plt.figure(2)
+sig2 = STN.pop_act[np.argmax(STN.pop_act[int(t_mvt/dt):int((t_mvt+D_mvt)/dt)])+int(t_mvt/dt):duration_mvt[1]]
+sig2 = STN.pop_act[np.argmax(STN.pop_act[0:int(t_mvt/dt)]):int(t_mvt/dt)]
+plt.figure()
 plt.plot(sig2)
-plt.plot(sig2[cut_plateau(sig2)])
-plt.plot(sig1[cut_plateau(sig1)])
+plt.plot(sig2-np.average(sig2))
+plt.plot(sig2[cut_plateau(sig2-np.average(sig2))])
+max_non_empty_array(cut_plateau(sig2-np.average(sig2)))/len(sig2)
+#derivative = np.diff(sig2-np.average(sig2))
+#derivative_2 = np.diff(derivative)
+#der_avg = moving_average_array(derivative, 10)
+#plt.plot(derivative)
+#plt.plot(np.diff(derivative))
+
+#peaks, vallies = signal.find_peaks(derivative, prominence = (0.2))
+
+np.max(cut_plateau(sig2-np.average(sig2)))
+
+#plt.plot(sig1[cut_plateau(sig1)])
 #plt.plot(sig_filtered)
 #fs = 1/(dt/1000)
 #[ff,pxx] = signal.welch(sig2,axis=0,nperseg=int(fs),fs=fs)#,nfft=1024)
@@ -433,9 +525,9 @@ plt.plot(sig1[cut_plateau(sig1)])
 #ff[np.argmax(pxx)]
 
 freq_from_fft((sig2-np.average(sig2))[[cut_plateau(sig2)]],dt/1000)
-freq_from_welch((sig2[10:]-np.average(sig2[10:]))[[cut_plateau(sig2[10:])]],dt/1000)
-freq_from_fft(sig2[cut_plateau(sig2)],dt)
-freq_from_welch(sig2[cut_plateau(sig2)],dt)
+#freq_from_welch((sig2[10:]-np.average(sig2[10:]))[[cut_plateau(sig2[10:])]],dt/1000)
+#freq_from_fft(sig2[cut_plateau(sig2)],dt)
+#freq_from_welch(sig2[cut_plateau(sig2)],dt)
 #f1 = fft(sig1)
 #f2 = fft(sig2)
 #plt.figure(2)
