f965e3bc280cc2c93bc2a7d5c8442eb3e9ff71f4
Shiva <you@example.com>
2021-01-27 10:57:33 +0100

    moved to DD experimental constants

--- Oscillation.py
+++ Oscillation.py
@@ -17,8 +17,11 @@ N_real = { 'STN': 13560 , 'GPe': 34470, 'GPi': 3200, 'Str': N_Str, 'D2': int(0.4
 A = { 'STN': 15 , 'GPe': 30,
      'FSI': 12.5, # FSI average firing rates:10–15 Hz. 60–80 Hz during behavioral tasks(Berke et al., 2004; Berke, 2008) or 18.5 Hz Berke et al 2010?
      'D1': 1.1, 'D2': 1.1, #Berke et al. 2010
+     'GPi':0} # mean firing rate from experiments
+A_DD = { 'STN': 0 , 'GPe': 0,
+     'FSI': 0, # FSI average firing rates:10–15 Hz. 60–80 Hz during behavioral tasks(Berke et al., 2004; Berke, 2008) or 18.5 Hz Berke et al 2010?
+     'D1': 6.6, 'D2': 6.6, # Kita & Kita. 2011
      'GPi':0} 
-# mean firing rate from experiments
 A_mvt = { 'STN': 50 , 'GPe': 22, 'FSI': 70} # mean firing rate during movement from experiments
 threshold = { 'STN': .1 ,'GPe': .1}
 neuron_type = {'STN': 'Glut', 'GPe': 'GABA'}
@@ -35,39 +38,87 @@ K_real = { ('STN', 'GPe'): 883, # Baufreton et al. (2009)
            ('FSI', 'GPe'): 360, # averaging the FSI contacting of GPe boutons Bevan 1998
            ('D1', 'FSI'): int(36*2/(36+53)*240),#Guzman et al  (2003): 240 from one class interneuron to each MSI # 36% (FSI-D1) Gittis et al.2010
            ('D2', 'FSI'): int(53*2/(36+53)*240),#Guzman et al  (2003): 240 from one class interneuron to each MSI # 53% (FSI-D2) Gittis et al.2010
-           ('FSI','FSI'): int(N_real['FSI']*0.58), # Gittis et al.
+           ('FSI','FSI'): int(N_real['FSI']*0.58), # Gittis et al. (2010)
            ('GPe', 'D2'): int(N_Str/2*(1-np.power(0.13,1/(.1*N_Str/2)))), # Chuhma et al. 2011 --> 10% MSN activation leads to 87% proto activation
+           ('D1', 'Ctx'):0,
+           ('D2', 'Ctx'):0,
+           ('FSI', 'Ctx'):0,
            ('GPi', 'D1'): 0,
            ('GPi', 'STN'): 0,
+           ('GPi', 'GPe'): 0,
            ('D1', 'D2'): Str_connec[('MSN','MSN')]*Str_connec[('D1', 'D2')]/(Str_connec[('D1', 'D2')]+Str_connec[('D2', 'D2')]),
            ('D2', 'D2'): Str_connec[('MSN','MSN')]*Str_connec[('D2', 'D2')]/(Str_connec[('D1', 'D2')]+Str_connec[('D2', 'D2')]),
            ('D1', 'D1'): Str_connec[('MSN','MSN')]*Str_connec[('D1', 'D1')]/(Str_connec[('D1', 'D1')]+Str_connec[('D2', 'D1')]),  
            ('D2', 'D1'): Str_connec[('MSN','MSN')]*Str_connec[('D2', 'D1')]/(Str_connec[('D1', 'D1')]+Str_connec[('D2', 'D1')]), #Guzman et al (2003) based on Taverna et al (2008)
            ('D1', 'GPe'): int(N_real['GPe']*(1-np.power(64/81, 1/N_real['GPe'])))} # Klug et al 2018
+
+K_real_DD = {('STN', 'GPe'):0, #883, # Baufreton et al. (2009)
+           ('GPe', 'STN'): 0,#190, # Kita, H., and Jaeger, D. (2016)
+           ('GPe', 'GPe'): 0,#650, # Hegeman et al. (2017)
+#           ('FSI', 'GPe'): int(800*15*N_real['GPe']/((0.04+0.457)*N_Str)*(0.75/(.75+.10+.54))), #800 boutons per GPe Guzman 2003, 15 contacts per bouton. 75%, 10%, 45%  connected to FSI, D1, NYP
+           ('FSI', 'GPe'): 0,#360, # averaging the FSI contacting of GPe boutons Bevan 1998
+           ('D1', 'FSI'): int(36*2/(36+53)*240),#Guzman et al  (2003): 240 from one class interneuron to each MSI # 36% (FSI-D1) Gittis et al.2010
+           ('D2', 'FSI'): 2*K_real[('D2', 'FSI')],
+           ('FSI','FSI'): int(N_real['FSI']*0.58), # Gittis et al. (2010)
+           ('GPe', 'D2'): int(N_Str/2*(1-np.power(0.13,1/(.1*N_Str/2)))), # Chuhma et al. 2011 --> 10% MSN activation leads to 87% proto activation
+           ('D1', 'Ctx'):0,
+           ('D2', 'Ctx'):0,
+           ('FSI', 'Ctx'):0,
+           ('GPi', 'D1'): 0,
+           ('GPi', 'STN'): 0,
+           ('GPi', 'GPe'): 0,
+           ('D1', 'D2'): 0.7*K_real[('D1', 'D2')], #Damodaran et al 2015 based on Taverna et al. 2008
+           ('D2', 'D2'): 0.5*K_real[('D2', 'D2')], #Damodaran et al 2015 based on Taverna et al. 2008
+           ('D1', 'D1'): 0, #Damodaran et al 2015 based on Taverna et al. 2008
+           ('D2', 'D1'): 0,
+           ('D1', 'GPe'): int(N_real['GPe']*(1-np.power(64/81, 1/N_real['GPe'])))} # Klug et al 2018
+
 K_real_STN_GPe_diverse = K_real.copy()
 K_real_STN_GPe_diverse[('GPe', 'STN')] = K_real_STN_GPe_diverse[('GPe', 'STN')] / N_sub_pop # because one subpop in STN contacts all subpop in GPe
-T = { ('STN', 'GPe'): 4, # Fujimoto & Kita (1993)
-      ('GPe', 'STN'): 2, # kita & Kitai (1991)
-      ('GPe', 'GPe'): 1.5, # estimate
-      ('GPe', 'Str'): 5, # Kita & Kitai (1991)
-      ('STN', 'Ctx'): 5.5, # kita & Kita (2011)
-      ('GPi', 'D1'): 7.2} # IPSP latency for now Kita et al. 2001
+T = { ('STN', 'GPe'): 4, # Fujimoto & Kita (1993) - [firing rate]
+      ('GPe', 'STN'): 2, # kita & Kitai (1991) - [firing rate]
+      ('GPe', 'GPe'): 5,#  Ketzef & Silberberg (2020)- [IPSP]/ in the begining was 1.5
+      ('GPe', 'D2'):  7.34, #ms proto Ketzef & Silberberg (2020) - [IPSP] /7ms Kita & Kitai (1991) - [IPSP]
+      ('STN', 'Ctx'): 5.5, # kita & Kita (2011) [firing rate]/ Fujimoto & Kita 1993 say an early excitaion of 2.5
+#      ('D2', 'Ctx'): 13.4 - 5, # short inhibition latency of MC--> GPe Kita & Kita (2011) - D2-GPe of Kita & Kitai (1991)
+      ('D2', 'Ctx'): 10.5, # excitation of MC--> Str Kita & Kita (2011) - [firing rate]
+      ('D1', 'Ctx'): 10.5,
+      ('FSI', 'Ctx'): 8/12.5 * 10.5 ,# Kita & Kita (2011) x FSI/MSN latency in SW- Mallet et al. 2005
+      ('GPi', 'D1'): 7.2, #  Kita et al. 2001 - [IPSP]
+      ('GPi', 'STN'): 1.7, #  STN-EP Nakanishi et al. 1991 [EPSP] /1ms # STN-SNr Nakanishi et al 1987 / 6 - 5.5  (early excitaion latency of MC--> GPi Kita & Kita (2011) - Ctx-STN) - [firing rate]
+      ('GPi', 'GPe'): 4 - 2, # Nakanishi et al. 1991: the IPSP following the EPSP with STN activation in EP, supposedly being due to STN-GPe-GPi circuit?
+      ('Ctx', 'GPi'): 0,
+      ('FSI', 'GPe'): 0,
+      ('D1' , 'FSI'): 0.84, #mice Gittis et al 2010
+      ('D2' , 'FSI'): 0.93, # mice Gittis et al 2010
+      ('FSI' , 'FSI'): 1, # estimate based on proximity
+      ('D2', 'D1'): 1,
+      ('D1', 'D1'): 1,
+      ('D1', 'D2'): 1,
+      ('D2', 'D2'): 1} 
     # transmission delay in ms
-G = { ('STN', 'GPe'): -2 ,
-      ('GPe', 'STN'): 0.5 , 
-      ('GPe', 'GPe'): 0,
-      ('Str', 'Ctx'): 0,
-      ('D2','GPe'): 0} # synaptic weight
+T_DD = {('D2', 'Ctx'): 5.5, # excitation of MC--> Str Kita & Kita (2011) 
+        ('D1', 'Ctx'): 5.5,
+        ('STN', 'Ctx'): 5.9} # kita & Kita (2011)
+G = {('STN', 'GPe'): -2 ,
+     ('GPe', 'STN'): 0.5 , 
+     ('GPe', 'GPe'): 0,
+     ('Str', 'Ctx'): 0,
+     ('D2','GPe'): 0,
+     ('D2', 'FSI'): 0, 
+     ('FSI', 'GPe'): 0,
+     ('FSI', 'FSI'): 0,
+     ('D2','D2'): 0
+     } # synaptic weight
 G[('GPe', 'GPe')] = 0.5* G[('STN', 'GPe')]
-G_DD = { ('STN', 'GPe'): -2 ,
+G[('D1', 'D1')] = 0.5* G[('D2', 'D2')]
+G_DD = {('STN', 'GPe'): -2 ,
       ('GPe', 'STN'): 0.5 , 
       ('GPe', 'GPe'): 0,
       ('Str', 'Ctx'): 0,
       ('D2','GPe'): G[('D2','GPe')]*108/28} # IPSP amplitude in Ctr: 28pA, in DD: 108pA Corbit et al. (2016) [Is it due to increased connections or increased synaptic gain?]
 G_DD[('GPe', 'GPe')] = 0.5* G_DD[('STN', 'GPe')]
-receiving_pop_list = {('STN','1') : [('GPe', '1')], ('STN','2') : [('GPe', '2')],
-                        ('GPe','1') : [('GPe', '1'), ('STN', '1'), ('STN', '2')],
-                        ('GPe','2') : [('GPe', '2'), ('STN', '1'), ('STN', '2')]}
+
 tau = {'GABA-A' : 6, 'GABA-B': 200, 'Glut': 3.5} # Gerstner. synaptic time scale for excitation and inhibition
 noise_variance = {'GPe' : 0.1, 'STN': 0.1}
 noise_amplitude = {'GPe' : 50, 'STN': 50}
@@ -490,13 +541,16 @@ def rolling_window(a, window):
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 
-#%%  
+#%% STN-GPe network
 #G = { ('STN', 'GPe'): g_mat[15,1] ,
 #  ('GPe', 'STN'): g_mat[15,0], 
 #  ('GPe', 'GPe'): 0} # synaptic weight
 ##G[('GPe', 'GPe')] = 0.5* G[('STN', 'GPe')]
 
 #K = calculate_number_of_connections(N,N_real,K_real)
+receiving_pop_list = {('STN','1') : [('GPe', '1')], ('STN','2') : [('GPe', '2')],
+                    ('GPe','1') : [('GPe', '1'), ('STN', '1'), ('STN', '2')],
+                    ('GPe','2') : [('GPe', '2'), ('STN', '1'), ('STN', '2')]}
 K = calculate_number_of_connections(N,N_real,K_real_STN_GPe_diverse)
 
 GPe = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'GPe', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list),
@@ -504,6 +558,27 @@ GPe = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'GPe', G, T, t_sim, dt,
 STN = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'STN', G, T, t_sim, dt, tau, ['Glut'], rest_ext_input, receiving_pop_list),
        Nucleus(2, noise_variance, noise_amplitude, N, A, 'STN', G, T, t_sim, dt, tau, ['Glut'], rest_ext_input, receiving_pop_list)]
 nuclei_dict = {'GPe': GPe, 'STN' : STN}
+
+receiving_class_dict = {key: None for key in receiving_pop_list.keys()}
+for key in receiving_class_dict.keys():
+    receiving_class_dict[key] = [nuclei_dict[name][int(k)-1] for name,k in list(receiving_pop_list[key])]
+
+run(mvt_selective_ext_input_dict, D_perturb,t_mvt,T, receiving_class_dict,t_list, K, N, threshold, gain, nuclei_dict)
+plot(GPe, STN, dt, t_list, A, A_mvt, t_mvt, D_mvt,plot_ob = None)
+#%% GPe-FSI-D2 network
+receiving_pop_list = {('FSI','1') : [('GPe', '1')], ('FSI','2') : [('GPe', '2')],
+                    ('GPe','1') : [('GPe', '1'), ('D2', '1')],
+                    ('GPe','2') : [('GPe', '2'), ('D2', '1')],
+                    ('D2','1') : [('FSI','1')], ('D2','2') : [('FSI','2')]}
+K = calculate_number_of_connections(N,N_real,K_real_STN_GPe_diverse)
+
+GPe = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'GPe', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list),
+       Nucleus(2, noise_variance, noise_amplitude, N, A, 'GPe', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list)]
+D2 = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'D2', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list),
+       Nucleus(2, noise_variance, noise_amplitude, N, A, 'D2', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list)]
+FSI = [Nucleus(1, noise_variance, noise_amplitude, N, A, 'FSI', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list),
+       Nucleus(2, noise_variance, noise_amplitude, N, A, 'FSI', G, T, t_sim, dt, tau, ['GABA-A'], rest_ext_input, receiving_pop_list)]
+nuclei_dict = {'GPe': GPe, 'D2' : D2}
 
 receiving_class_dict = {key: None for key in receiving_pop_list.keys()}
 for key in receiving_class_dict.keys():
